497b3add23b16bfedb6d5c4cc471c7b925ebda8f
Monk191173 <129600062+Monk191173@users.noreply.github.com>
2023-06-17 21:04:40 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,14 +5,14 @@ $(document).ready(function () {
         $('#text').html('1<br>minute');
         $('.polosa').slideUp(60000, 'linear', () => {
             $('#text').html('0<br>minute');
+
+            let str = $('span[id="s1"]').text() +
+                $('b.s2').text() +
+                $('#second i:first').text() +
+                $('q[title="hello"]').text() +
+                $('a[target="_blank"]').text() +
+                $('.extra').eq(1).text()
+            alert(str);
         });
     });
-
-    let str = $('span[id="s1"]').text() +
-        $('b.s2').text() +
-        $('#second i:first').text() +
-        $('q[title="hello"]').text() +
-        $('a[target="_blank"]').text() +
-        $('.extra').eq(1).text()
-    console.log(str);
 });
